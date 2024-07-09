ed1ce6d935f75028c23c3a9b9a4ac372e1c34662
Ryan <ryansouzadev@gmail.com>
2024-07-09 09:06:46 -0300

    feat: add navigation drawer

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CordarSe - Todos os cursos",
+};
+
 export default function PageCursos() {
   return <main>Cursos</main>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CordarSe - Página inicial",
+};
+
 export default function PageHome() {
   return <main className="bg-background">Home</main>;
 }
