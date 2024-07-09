7ac0c0354f646b0e4686554464f1c1beb386e254
Ryan <ryansouzadev@gmail.com>
2024-07-09 08:35:47 -0300

    feat: add responsive header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,20 +2,24 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { MdOutlineOpenInNew } from "react-icons/md";
+import { MdMenu, MdOutlineOpenInNew } from "react-icons/md";
 
 export const Header = () => {
   const currentPath = usePathname();
 
   return (
-    <nav className="flex items-center justify-center bg-primary py-4">
+    <nav className="flex items-center gap-6 justify-start md:justify-center px-6 bg-primary py-2 sm:py-4">
+      <button className="sm:hidden">
+        <MdMenu size={24} />
+      </button>
+
       <ul className="flex gap-4 items-center">
         <li className="my-2">
           <Link href="/" className="border-2 rounded-md py-2 px-1 font-bold">
             CODARSE
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link
             href="/"
             data-active={currentPath === "/"}
@@ -24,7 +28,7 @@ export const Header = () => {
             Página Inicial
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link
             href="/cursos"
             data-active={currentPath === "/cursos"}
@@ -33,7 +37,7 @@ export const Header = () => {
             Cursos
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link
             href="https://blog.codarse.com"
             target="_blank"
@@ -44,6 +48,8 @@ export const Header = () => {
           </Link>
         </li>
       </ul>
+
+      <h1 className="sm:hidden">CordarSe - Página inicial</h1>
     </nav>
   );
 };
